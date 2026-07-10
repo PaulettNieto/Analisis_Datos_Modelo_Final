3332be800d4f417176da5db912f5e5dc1fd9b102
Ivonne Paola Nieto C. <coffepaola072002@gmail.com>
2026-07-09 20:57:38 -0500

    Create analisis_datos.py

---
+++ Código/analisis_datos.py
@@ -0,0 +1,3 @@
+# Análisis de datos y ajuste de modelo final
+# Este archivo contiene el proceso de carga, preparación,
+# análisis exploratorio y evaluación de resultados del conjunto de datos asignado.
